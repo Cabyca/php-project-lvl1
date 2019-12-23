b556fd6cf53254fa913a8b6b3b81a1a2875b7724
Alex <cabyca@yandex.ru>
2019-12-23 22:10:20 +0300

    Seven step two

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -7,7 +7,7 @@ function gcd()
     $randomValue1 = mt_rand(1, 25);
     $randomValue2 = mt_rand(1, 25);
     
-    $minNumber = min ($randomValue1, $randomValue2);
+    $minNumber = min($randomValue1, $randomValue2);
 
     while (true) {
         if ($randomValue1 % $minNumber === 0 && $randomValue2 % $minNumber === 0) {
