fb24e68adbd41eeea6f13078a23dbfe5163af872
Alex <cabyca@yandex.ru>
2019-12-24 22:33:14 +0300

    Eighth game two step

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -4,7 +4,7 @@ namespace BrainGames\Progression;
 
 function progression()
 {
-    $firstDigitOfProgression = mt_rand(1, 10);    
+    $firstDigitOfProgression = mt_rand(1, 10);
     $progressionArray[0] = $firstDigitOfProgression;
     
     $progressionStep = mt_rand(1, 10);
@@ -12,8 +12,7 @@ function progression()
     $indexNumber = mt_rand(0, 9);
     
     for ($i = 1; $i < 10; $i += 1) {
-        $progressionArray[$i] = $progressionArray[$i -1] + $progressionStep;
-        
+        $progressionArray[$i] = $progressionArray[$i - 1] + $progressionStep;
     }
    
     $textCorrectAnswer = (string) $progressionArray[$indexNumber];
